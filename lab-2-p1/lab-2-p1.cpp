dc72881e7097a94ad9e7d24bb0f5b38f1e0951c3
Sergey <Sergey@DESKTOP-ME9HMT2>
2023-09-11 23:22:25 +0700

    Добавьте файлы проекта.

---
+++ lab-2-p1/lab-2-p1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+	const double pi = atan(1) * 4;
+	double L;
+	std::cout << "L = ";
+	std::cin >> L;
+	double R = L / (2*pi);
+	double S = pi * pow(R, 2);
+	std::cout << "R = " << R << "; S = " << S << std::endl;
+}
